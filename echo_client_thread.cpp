9317e914740c915c78561ab2b471d54951ba2647
tusher002 <tm20du@my.fsu.edu>
2021-04-01 00:43:45 -0400

    modified threaded server

--- echo_client_thread.cpp
+++ echo_client_thread.cpp
@@ -37,15 +37,26 @@ void *process_connection(void *arg) {
 
 //const unsigned serv_port = 5100;
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+    char buf[MAXBUFLEN];
+	char buf1[MAXBUFLEN];
+	char buffer[MAXBUFLEN];
+	char buffer1[MAXBUFLEN];
+	char output[MAXBUFLEN];
+    int maxf, maxf1;
+	fd_set  rset, orig_set;
+	fd_set  rset1, orig_set1;
+	ssize_t n, m;
     int rv, flag;
-    ssize_t n;
+
     struct addrinfo hints, *res, *ressave;
     pthread_t tid;
 
-    if (argc != 3) {
-	cout << "echo_client server_name_or_ip port" << endl;
-	exit(1);
+    if (argc != 3)
+    {
+        cout << "echo_client server_name_or_ip port" << endl;
+        exit(1);
     }
 
     cout << argv[1] << " " << argv[2] << endl;
@@ -53,41 +64,180 @@ int main(int argc, char **argv) {
     bzero(&hints, sizeof(struct addrinfo));
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    
-    if ((rv = getaddrinfo(argv[1], argv[2], &hints, &res)) != 0) {
-	cout << "getaddrinfo wrong: " << gai_strerror(rv) << endl;
-	exit(1);
+
+    if ((rv = getaddrinfo(argv[1], argv[2], &hints, &res)) != 0)
+    {
+        cout << "getaddrinfo wrong: " << gai_strerror(rv) << endl;
+        exit(1);
     }
 
     ressave = res;
     flag = 0;
-    do {
+    do
+    {
 	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-	if (sockfd < 0) 
+	if (sockfd < 0)
 	    continue;
 	if (connect(sockfd, res->ai_addr, res->ai_addrlen) == 0) {
 	    flag = 1;
 	    break;
 	}
 	close(sockfd);
-    } while ((res = res->ai_next) != NULL);
+    }
+    while ((res = res->ai_next) != NULL);
     freeaddrinfo(ressave);
 
-    if (flag == 0) {
-	fprintf(stderr, "cannot connect\n");
-	exit(1);
+    if (flag == 0)
+    {
+        fprintf(stderr, "cannot connect\n");
+        exit(1);
     }
 
     pthread_create(&tid, NULL, &process_connection, NULL);
 
-    string oneline;
-    while (getline(cin, oneline)) {
-	if (oneline == "quit") {
-	    close(sockfd);
-	    break;
-	} else {
-	    write(sockfd, oneline.c_str(), oneline.length());
-	}
+    FD_ZERO(&orig_set);
+    FD_SET(STDIN_FILENO, &orig_set);
+    FD_SET(sockfd, &orig_set);
+    if (sockfd > STDIN_FILENO) maxf = sockfd+1;
+    else maxf = STDIN_FILENO+ 1;
+
+    while (1)
+    {
+        rset = orig_set;
+        select(maxf, &rset, NULL, NULL, NULL);
+        if (FD_ISSET(sockfd, &rset))
+        {
+            if (read(sockfd, buf, MAXBUFLEN) == 0)
+            {
+                printf("Server crashed.\n");
+                close(sockfd);
+                exit(0);
+            }
+            printf("Server response : %s\n", buf);
+        }
+
+        if (FD_ISSET(STDIN_FILENO, &rset))
+        {
+            if (fgets(buffer, MAXBUFLEN, stdin) == NULL)
+                {
+                    close(sockfd);
+                    exit(0);
+                }
+
+            string oneline(buffer);
+            if (oneline == "exit")
+            {
+                close(sockfd);
+                return 0;
+            }
+            else
+            {
+                string command;
+                command = oneline;
+                command = command + " ";
+                string cmd1, cmd, cmd2;
+                cmd = command;
+                cmd1 = "";
+                cmd2 = "";
+                int pos1 = command.find(" ");
+                command.pop_back();
+                int len = command.length();
+                cmd1 = cmd.erase(pos1, len);
+                cmd2 = command.erase(0, pos1+1);
+
+                if(cmd1 == "login")
+                {
+                    write(sockfd, oneline.c_str(), oneline.length());
+                    n = read(sockfd, buf, MAXBUFLEN);
+                    if (n <= 0)
+                    {
+                        if (n == 0)
+                        {
+                            cout << "Server closed" << endl;
+                        } else
+                        {
+                            cout << "something wrong" << endl;
+                        }
+                        close(sockfd);
+                        exit(0);
+                    }
+
+                    buf[n] = '\0';
+                    cout << buf << endl;
+                    string command2;
+
+                    FD_ZERO(&orig_set);
+                    FD_SET(STDIN_FILENO, &orig_set);
+                    FD_SET(sockfd, &orig_set);
+                    if (sockfd > STDIN_FILENO) maxf = sockfd+1;
+                    else maxf = STDIN_FILENO+ 1;
+                    while (1)
+                    {
+                        rset = orig_set;
+                        select(maxf, &rset, NULL, NULL, NULL);
+                        if (FD_ISSET(sockfd, &rset))
+                        {
+                            if (read(sockfd, buf1, MAXBUFLEN) == 0)
+                            {
+                                printf("Server crashed.\n");
+                                close(sockfd);
+                                exit(0);
+                            }
+                            printf("Server response : %s\n", buf);
+                        }
+                        else if (FD_ISSET(STDIN_FILENO, &rset))
+                        {
+                            if (fgets(buffer1, MAXBUFLEN, stdin) == NULL)
+                            {
+                                close(sockfd);
+                                exit(0);
+                            }
+                            string command2(buffer1);
+                            string cmd_nxt;
+                            cmd_nxt = command2;
+                            cmd_nxt = cmd_nxt + " ";
+                            string cmd1_nxt;
+                            cmd1_nxt = "";
+                            int pos_next = cmd_nxt.find(" ");
+                            int len2 = cmd_nxt.length();
+                            cmd1_nxt = cmd_nxt.erase(pos_next, len2);
+
+                            if(cmd1_nxt == "logout")
+                            {
+                                command2 = command2 + " " + cmd2;
+                                write(sockfd, command2.c_str(), command2.length());
+                                m = read(sockfd, output, MAXBUFLEN);
+                                if (m <= 0)
+                                {
+                                    if (m == 0)
+                                    {
+                                        cout << "server closed" << endl;
+                                    } else
+                                    {
+                                        cout << "something wrong" << endl;
+                                    }
+                                    close(sockfd);
+                                    exit(0);
+                                }
+
+                                output[m] = '\0';
+                                cout << output << endl;
+                                break;
+                            }
+                            else
+                            {
+                                cout<<"doing well-----------"<<endl;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    cout<<"You need to log in first"<<endl;
+                }
+            }
+        }
+
     }
     exit(0);
 }
--- echo_server_thread.cpp
+++ echo_server_thread.cpp
@@ -11,10 +11,12 @@
 
 using namespace std;
 
-const unsigned port = 5100;
+map<string, int> user_info;
+int port;
 const unsigned MAXBUFLEN = 512;
 
-void *process_connection(void *arg) {
+void *process_connection(void *arg)
+{
     int sockfd;
     ssize_t n;
     char buf[MAXBUFLEN];
@@ -23,21 +25,94 @@ void *process_connection(void *arg) {
     free(arg);
 
     pthread_detach(pthread_self());
-    while ((n = read(sockfd, buf, MAXBUFLEN)) > 0) {
-	buf[n] = '\0';
-	cout << buf << endl;
-	write(sockfd, buf, strlen(buf));
+    while ((n = read(sockfd, buf, MAXBUFLEN)) > 0)
+    {
+        buf[n] = '\0';
+        //cout << buf << endl;
+        string command(buf);
+        command = command + " ";
+        string cmd1, cmd2, cmd;
+        cmd = command;
+        cmd1 = "";
+        cmd2 = "";
+        int pos1 = command.find(" ");
+        command.pop_back();
+        int n = command.length();
+        cmd2 = command.erase(0, pos1+1);
+        cmd1 = cmd.erase(pos1, n);
+        if(cmd1 == "login")
+        {
+            user_info.insert(pair<string, int>(cmd2, cli_sockfd));
+            cout << "Logged In User:"<<endl;
+            map<string, int>::iterator user_it = user_info.begin();
+            while(user_it != user_info.end())
+            {
+                cout << '\t' << user_it->first<< '\t' << user_it->second << '\n';
+                user_it++;
+            }
+            cout << endl;
+
+            string reply;
+            reply = "Welcome " + cmd2 + ", you have successfully logged in";
+            write(cli_sockfd, reply.c_str(), reply.length());
+        }
+        else if(cmd1 == "logout")
+        {
+            user_info.erase(user_info.find(cmd2));
+            map<string, int>::iterator user_it = user_info.begin();
+            cout << "Logged In User:"<<endl;
+            while(user_it != user_info.end())
+            {
+                cout << '\t' << user_it->first<< '\t' << user_it->second << '\n';
+                user_it++;
+            }
+            cout << endl;
+
+            string reply;
+            reply = "Welcome " + cmd2 + ", you have successfully logged in";
+            write(cli_sockfd, reply.c_str(), reply.length());
+        }
+        memset(buf, 0, sizeof buf);
     }
-    if (n == 0) {
-	cout << "client closed" << endl;
-    } else {
-	cout << "something wrong" << endl;
+    if (n == 0)
+    {
+        cout << "client closed" << endl;
+    }
+    else
+    {
+        cout << "something wrong" << endl;
     }
     close(sockfd);
     return(NULL);
 }
 
-int main() {
+int main()
+{
+    fstream my_file;
+    my_file.open("configuration_file.txt", ios::in);
+    if (!my_file)
+    {
+        cout << "File not found"<<endl<<endl;
+    }
+    else
+    {
+        regex reg("(port)(.*)");
+        string line;
+        while ( getline (my_file, line) )
+        {
+          if ( regex_match(line, reg) )
+          {
+            string delimiter = ":";
+            size_t pos = 0;
+            string token;
+            while ((pos = line.find(delimiter)) != std::string::npos) {
+                line.erase(0, pos + delimiter.length());
+                port = stoi(line);
+            }
+          }
+        }
+        my_file.close();
+    }
     int serv_sockfd, cli_sockfd, *sock_ptr;
     struct sockaddr_in serv_addr, cli_addr;
     socklen_t sock_len;
@@ -45,27 +120,28 @@ int main() {
 
     cout << "port = " << port << endl;
     serv_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    
+
     bzero((void*)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(port);
 
     bind(serv_sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-    
+
     listen(serv_sockfd, 5);
 
-    for (; ;) {
-	sock_len = sizeof(cli_addr);
-	cli_sockfd = accept(serv_sockfd, (struct sockaddr *)&cli_addr, &sock_len);
-	
-	cout << "remote client IP == " << inet_ntoa(cli_addr.sin_addr);
-	cout << ", port == " << ntohs(cli_addr.sin_port) << endl;
+    for (; ;)
+    {
+        sock_len = sizeof(cli_addr);
+        cli_sockfd = accept(serv_sockfd, (struct sockaddr *)&cli_addr, &sock_len);
 
-	sock_ptr = (int *)malloc(sizeof(int));
-	*sock_ptr = cli_sockfd;
-	
-	pthread_create(&tid, NULL, &process_connection, (void
-							 *)sock_ptr);
+        cout << "remote client IP == " << inet_ntoa(cli_addr.sin_addr);
+        cout << ", port == " << ntohs(cli_addr.sin_port) << endl;
+
+        sock_ptr = (int *)malloc(sizeof(int));
+        *sock_ptr = cli_sockfd;
+
+        pthread_create(&tid, NULL, &process_connection, (void
+                                 *)sock_ptr);
     }
 }
